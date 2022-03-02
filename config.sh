cd0c518b9ea6216668e5cc5673ed0a0214021948
fudute <49638038+fudute@users.noreply.github.com>
2022-03-03 01:08:11 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,3 +1,6 @@
+# 安装zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" -y
+
 # 安装docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh --mirror Aliyun
@@ -5,13 +8,21 @@ sudo sh get-docker.sh --mirror Aliyun
 sudo usermod -aG docker ubuntu
 echo "请重新登录使新增用户组生效"
 
-# 安装minikube
+# 安装minikube,kubectl
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# minikube， kubectl auto completion
+echo 'source <(minikube completion zsh)' >> $HOME/.zshrc
+echo 'source <(minikube kubectl zsh)' >> $HOME/.zshrc
+
 # 运行minikube
 minikube start --image-mirror-country='cn' --logtostderr
 
 # 安装golang
 wget https://dl.google.com/go/go1.17.7.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.17.7.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.profile && source .profile
+echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.zshrc && source .zshrc
